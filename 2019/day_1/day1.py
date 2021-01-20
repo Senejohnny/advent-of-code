642c09deb3a68bf46289e5e8d2355f9c96a6ed1e
Danial <d.senejohnny@gmail.com>
2021-01-20 22:34:17 +0100

    pre commits are added

--- 2019/day_1/day1.py
+++ 2019/day_1/day1.py
@@ -12,7 +12,7 @@ def calculate_fule(mass, _sum=0):
     fule = mass // 3 - 2 
     if fule > 0:
         _sum += fule
-        return calculate_fule(fule, _sum)
+        return calculate_fule(fule, _sum) 
     else:
         return _sum
 
