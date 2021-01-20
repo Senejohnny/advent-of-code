9dbd8a268d7451e7dc13fc3a88668eff799611a3
Danial <d.senejohnny@gmail.com>
2021-01-20 23:01:30 +0100

    push to repo

--- 2019/day_1/day1.py
+++ 2019/day_1/day1.py
@@ -1,6 +1,6 @@
 """ day 1 of advent of code """
 import os
-path = os.path.abspath('./day_1/input_day1.txt') 
+path = os.path.abspath('./day_1/input_day1.txt')
 with open(path) as file:
     modules_masses = [x.strip() for x in file.read().splitlines()]
 
@@ -9,11 +9,11 @@ print('Part 1:', sum([(int(mass) // 3 - 2) for mass in modules_masses]))
 
 # Part 2
 def calculate_fule(mass, _sum=0):
-    fule = mass // 3 - 2 
+    """ function that calculates spent fule """
+    fule = mass // 3 - 2
     if fule > 0:
         _sum += fule
-        return calculate_fule(fule, _sum)   
-    else:
-        return _sum
+        return calculate_fule(fule, _sum)
+    return _sum
 
 print('Part 2:', sum([calculate_fule(int(mass)) for mass in modules_masses]))
